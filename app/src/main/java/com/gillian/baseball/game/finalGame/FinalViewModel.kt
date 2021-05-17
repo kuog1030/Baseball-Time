a522cb3bf666ba78ebc58771080af28e48254596
Gillian Kuo <kuog1030@gmail.com>
2021-05-17 22:28:08 +0800

    Hard code final score box

--- app/src/main/java/com/gillian/baseball/game/finalGame/FinalViewModel.kt
+++ app/src/main/java/com/gillian/baseball/game/finalGame/FinalViewModel.kt
@@ -9,34 +9,10 @@ import com.gillian.baseball.data.source.BaseballRepository
 
 class FinalViewModel(private val repository: BaseballRepository, private val myGame: MyGame) : ViewModel() {
 
-    val scoreBox = MutableLiveData<String>()
-    var scoreText = ""
-    var runText = ""
-    var hitText = ""
-    var errorText = ""
+    var score = myGame.game.box.score
 
+    var run = myGame.game.box.run
+    var hit = myGame.game.box.hit
+    var error = myGame.game.box.error
 
-    val runBox = MutableLiveData<String>()
-    val hitBox = MutableLiveData<String>()
-    val errorBox = MutableLiveData<String>()
-
-    init {
-
-        Log.i("gillian", "score box is ${myGame.game.box.score}")
-
-        for (score in myGame.game.box.score) {
-            scoreText += "$score - "
-        }
-
-        for (i in 0..1) {
-            runText += "${myGame.game.box.run[i]} -"
-            hitText += "${myGame.game.box.hit[i]} -"
-            errorText += "${myGame.game.box.error[i]} -"
-        }
-
-        scoreBox.value = scoreText
-        runBox.value = runText
-        hitBox.value = hitText
-        errorBox.value = errorText
-    }
 }
