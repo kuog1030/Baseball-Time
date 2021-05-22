cb2e2c1e0e3d47a8f0555ae869fab09e46e7ce63
Gillian Kuo <kuog1030@gmail.com>
2021-05-22 14:05:38 +0800

    Modify layout of order fragment

--- app/src/main/java/com/gillian/baseball/ext/Ext.kt
+++ app/src/main/java/com/gillian/baseball/ext/Ext.kt
@@ -3,6 +3,7 @@ package com.gillian.baseball.ext
 import android.util.Log
 import com.gillian.baseball.data.Event
 import com.gillian.baseball.data.HitterBox
+import com.gillian.baseball.data.PitcherBox
 import com.gillian.baseball.game.EventType
 import com.gillian.baseball.login.UserManager
 
@@ -120,3 +121,139 @@ fun List<Event>.toPersonalScore() : List<String> {
 
     return result
 }
+
+//
+//fun List<Event>.toGameStat(isHome: Boolean) {
+//
+//
+//    val guestPitcher = mutableListOf(PitcherBox())
+//    val homePitcher = mutableListOf(PitcherBox())
+//    val guestHitter = mutableListOf(HitterBox())
+//    val homeHitter = mutableListOf(HitterBox())
+//
+//    fun updateHitterBox(event: Event, box: HitterBox) {
+//        val type = event.result
+//        lateinit var targetEventType: EventType
+//
+//        for (eventType in EventType.values()) {
+//            if (type == eventType.number) {
+//                targetEventType = eventType
+//                break
+//            }
+//        }
+//
+//        // TODO() 之後得分的result要改掉，目前傳上去是0
+//        if (type == 0) targetEventType = EventType.RUN
+//
+//        if (targetEventType.isAtBat) box.atBat += 1
+//        box.run += event.run
+//        box.runsBattedIn += event.rbi
+//
+//        when (targetEventType) {
+//            EventType.SINGLE -> box.hit += 1
+//            EventType.DOUBLE -> box.hit += 1
+//            EventType.TRIPLE -> box.hit += 1
+//            EventType.HOMERUN -> box.hit += 1
+//            EventType.DROPPEDTHIRD -> box.strikeOut += 1
+//            EventType.STRIKEOUT -> box.strikeOut += 1
+//            EventType.WALK -> box.baseOnBalls += 1
+//            EventType.STEALBASE -> box.stealBase += 1
+//            else -> null
+//        }
+//    }
+//
+//    fun updatePitcherBox(event: Event, box: PitcherBox) {
+//        val type = event.result
+//        lateinit var targetEventType: EventType
+//
+//        for (eventType in EventType.values()) {
+//            if (type == eventType.number) {
+//                targetEventType = eventType
+//                break
+//            }
+//        }
+//
+//        // TODO() 之後得分的result要改掉，目前傳上去是0
+//        if (type == 0) targetEventType = EventType.RUN
+//
+//        if (targetEventType.isAtBat) box.atBat += 1
+//        box.run += event.run
+//        box.runsBattedIn += event.rbi
+//
+//        when (targetEventType) {
+//            EventType.SINGLE -> box.hit += 1
+//            EventType.DOUBLE -> box.hit += 1
+//            EventType.TRIPLE -> box.hit += 1
+//            EventType.HOMERUN -> {
+//                box.hit += 1
+//                box.homerun += 1
+//            }
+//            EventType.RUN -> box.run += 1
+//            EventType.DROPPEDTHIRD -> box.strikeOut += 1
+//            EventType.STRIKEOUT -> box.strikeOut += 1
+//            EventType.WALK -> box.baseOnBalls += 1
+//            EventType.STEALBASE -> box.stealBase += 1
+//            else -> null
+//        }
+//
+//        //        var name : String? = "",
+//        //        val playerId : Long = 0L,
+//        //        var inningsPitched : Float = 0F,
+//        //        var hit : Int = 0,
+//        //        var run : Int = 0,
+//        //        var earnedRuns : Int = 0,
+//        //        var baseOnBalls : Int = 0,
+//        //        var strikeOut : Int = 0,
+//        //        var homerun : Int = 0,
+//
+//    }
+//
+//
+//    for (event in this) {
+//        // 上半局 打者event加進去guest那邊
+//        if (event.inning % 2 == 1) {
+//
+//
+//            var noHitter = true
+//            for (oneHitterBox in guestHitter) {
+//                if (oneHitterBox.playerId == event.player.playerId) {
+//                    updateHitterBox(event, oneHitterBox)
+//                    noHitter = false
+//                    break
+//                }
+//            }
+//
+//            if (noHitter) {
+//                val newHitterBox = HitterBox(name = event.player.name, playerId = event.player.playerId)
+//                updateHitterBox(event, newHitterBox)
+//                guestResult.add(newHitterBox)
+//            } else {
+//
+//            }
+//        } else {
+//            var noHitter = true
+//            for (oneHitterBox in homeResult) {
+//                // 找到我要的欄位 已經有這個球員了更新
+//                if (oneHitterBox.playerId == event.player.playerId) {
+//                    updateBox(event, oneHitterBox)
+//                    noHitter = false
+//                    break
+//                }
+//            }
+//
+//            if (noHitter) {
+//                val newHitterBox = HitterBox(name = event.player.name, playerId = event.player.playerId)
+//                updateBox(event, newHitterBox)
+//                homeResult.add(newHitterBox)
+//                Log.i("gillian", "home result加東西")
+//            }
+//        }
+//    }
+//
+//    //guestResult.removeAt(0)
+//    //homeResult.removeAt(0)
+//
+//    Log.i("gillian", "give me $guestResult and it size ${guestResult.size}" )
+//    Log.i("gillian", "also home $homeResult and it size ${homeResult.size}")
+//    return guestResult
+//}
